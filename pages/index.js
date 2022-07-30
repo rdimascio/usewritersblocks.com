01bb416b9d988bc552c7b35f54c74e8a168cbb78
Ryan DiMascio <ryan.dimascio@gmail.com>
2022-07-30 12:41:42 -0700

    fix: lint errors

--- pages/index.js
+++ pages/index.js
@@ -279,7 +279,7 @@ export default function Home() {
 									<button
 										className="px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md"
 										onClick={() => {
-											setTheme(theme === 'light' ? 'dark' : 'light')
+											setTheme(theme === 'light' ? 'dark' : 'light');
 										}}
 									>
 										Change Theme
@@ -323,8 +323,9 @@ export default function Home() {
 									been waiting for.
 								</h1>
 								<p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-									Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-									fugiat veniam occaecat fugiat aliqua.
+									Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+									lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+									fugiat aliqua.
 								</p>
 								<div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
 									<div className="rounded-md shadow">
