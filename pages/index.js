d69e61771e81e442e760f4af037d4763f4f40500
Ryan DiMascio <ryan.dimascio@gmail.com>
2022-08-18 15:39:56 -0700

    fix: lint errors

--- pages/index.js
+++ pages/index.js
@@ -196,8 +196,9 @@ export default function Home() {
 						been waiting for.
 					</h1>
 					<p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-						Write better with Writer's Blocks. Improve your clarity, readability, and efficiency with
-						the first inline content editor built specifically for the new block editor.
+						Write better with Writer's Blocks. Improve your clarity, readability, and
+						efficiency with the first inline content editor built specifically for the
+						new block editor.
 					</p>
 					<div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
 						<div className="rounded-md shadow">
